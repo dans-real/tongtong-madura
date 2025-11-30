c48f1e3e03510b0a9b08c1a2e33d8b229cf01e68
dhanisyafreal <danisyafreal123@gmail.com>
2025-11-30 21:45:51 +0700

    first final

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  // NAMA REPO di GitHub
-  basePath: "/tongtong-madura",
-  assetPrefix: "/tongtong-madura/",
+  // Kalau dev (npm run dev) → tanpa basePath
+  // Kalau di-build (GitHub Pages) → pakai nama repo
+  basePath: isProd ? "/tongtong-madura" : "",
+  assetPrefix: isProd ? "/tongtong-madura/" : "",
 };
 
 export default nextConfig;
